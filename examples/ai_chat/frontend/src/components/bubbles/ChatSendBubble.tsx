bc0efc5206049ec5a16b56e84ff7a6dda374e093
Matthew Rideout <boon4376@gmail.com>
2025-03-04 10:27:54 -0500

    Chat looking pretty great now.

--- examples/ai_chat/frontend/src/components/bubbles/ChatSendBubble.tsx
+++ examples/ai_chat/frontend/src/components/bubbles/ChatSendBubble.tsx
@@ -3,10 +3,10 @@ import { ChatBubbleProps } from './schemas'
 export default function ChatSendBubble(props: ChatBubbleProps) {
   const { message } = props
   return (
-    <div className="w-full my-5 flex justify-end">
-      <div className={'grow pl-[15%] max-w-xl pr-5 relative'}>
-        <div className="safe-word-break bg-gradient-to-b from-blue-600 to-blue-700 text-blue-100 rounded-3xl leading-tight px-6 py-5 relative">
-          <div className="absolute right-[-9px] bottom-5.5 w-0 h-0 border-t-[10px] border-t-transparent border-l-[10px] border-l-blue-600 border-b-[10px] border-b-transparent"></div>
+    <div className="w-full my-6 flex justify-end">
+      <div className={'pl-[15%] max-w-xl pr-5'}>
+        <div className="safe-word-break px-4 py-3 rounded-2xl bg-gradient-to-br from-blue-600 to-blue-700 text-blue-100 leading-tight relative">
+          <div className="absolute whitespace-pre-wrap right-[-9px] bottom-4 w-0 h-0 border-t-[10px] border-t-transparent border-l-[10px] border-l-blue-700 border-b-[10px] border-b-transparent"></div>
           {message.message}
         </div>
       </div>
