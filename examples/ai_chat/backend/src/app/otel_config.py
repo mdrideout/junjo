389f23effdf37da44ddd0dd69696215387befffd
Matthew Rideout <boon4376@gmail.com>
2025-05-04 16:44:11 -0400

    Junjo example updated for Junjo Server API key requirements, and new Jaeger simplification.

--- examples/ai_chat/backend/src/app/otel_config.py
+++ examples/ai_chat/backend/src/app/otel_config.py
@@ -1,3 +1,5 @@
+import os
+
 from junjo.telemetry.junjo_server_otel_exporter import JunjoServerOtelExporter
 from opentelemetry import metrics, trace
 from opentelemetry.sdk.metrics import MeterProvider
@@ -8,6 +10,12 @@ from opentelemetry.sdk.trace import TracerProvider
 def init_otel(service_name: str):
     """Configure OpenTelemetry for this application."""
 
+    # Load the JUNJO_SERVER_API_KEY from the environment variable
+    JUNJO_SERVER_API_KEY = os.getenv("JUNJO_SERVER_API_KEY")
+    if JUNJO_SERVER_API_KEY is None:
+        raise ValueError("JUNJO_SERVER_API_KEY environment variable is not set."
+                         "Generate a new API key in the Junjo Server UI.")
+
     # Configure OpenTelemetry for this application
     # Create the OpenTelemetry Resource to identify this service
     resource = Resource.create({"service.name": service_name})
@@ -15,35 +23,25 @@ def init_otel(service_name: str):
     # Set up tracing for this application
     tracer_provider = TracerProvider(resource=resource)
 
-    # Construct a Junjo exporter for Jaeger (see junjo-server docker-compose.yml)
-    junjo_jaeger_exporter = JunjoServerOtelExporter(
-        host="localhost",
-        port="4317",
-        api_key="your_api_key_here",
-        insecure=True,
-    )
-
     # Construct a Junjo exporter for Junjo Server (see junjo-server docker-compose.yml)
     junjo_server_exporter = JunjoServerOtelExporter(
         host="localhost",
         port="50051",
-        api_key="your_api_key_here",
+        api_key=JUNJO_SERVER_API_KEY,
         insecure=True,
     )
 
     # Set up span processors
     # Add the Junjo span processor (Junjo Server and Jaeger)
     # Add more span processors if desired
-    tracer_provider.add_span_processor(junjo_jaeger_exporter.span_processor)
     tracer_provider.add_span_processor(junjo_server_exporter.span_processor)
     trace.set_tracer_provider(tracer_provider)
 
     # Set up metrics
     #    - Construct with the Junjo metric reader (Junjo Server and Jaeger)
     #    - Add more metric readers if desired
-    junjo_jaeger_metric_reader = junjo_jaeger_exporter.metric_reader
     junjo_server_metric_reader = junjo_server_exporter.metric_reader
     meter_provider = MeterProvider(
-        resource=resource, metric_readers=[junjo_jaeger_metric_reader, junjo_server_metric_reader]
+        resource=resource, metric_readers=[junjo_server_metric_reader]
     )
     metrics.set_meter_provider(meter_provider)
