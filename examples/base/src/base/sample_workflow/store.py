070166417d30795d2d404db03bacc71311f66e0d
Matthew Rideout <boon4376@gmail.com>
2025-05-17 13:10:29 -0400

    Removed unneeded state from example.

--- examples/base/src/base/sample_workflow/store.py
+++ examples/base/src/base/sample_workflow/store.py
@@ -7,7 +7,6 @@ from junjo.store import BaseStore
 class SampleWorkflowState(BaseState):
     items: list[str]
     counter: int
-    decision: int | None = None
     joke: str | None = None
     fact: str | None = None
 
@@ -25,9 +24,6 @@ class SampleWorkflowStore(BaseStore[SampleWorkflowState]):
 
     async def add_ten(self, payload: Any = None) -> None:
         await self.set_state({"counter": self._state.counter + 10})
-
-    async def set_decision(self, payload: int) -> None:
-        await self.set_state({"decision": payload})
 
     async def set_joke(self, payload: str) -> None:
         await self.set_state({"joke": payload})
